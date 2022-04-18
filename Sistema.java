d2ebbdee5d7819b3ebb49b182671cbc4bb6ac33f
Saimon <100451976+CSParadiso@users.noreply.github.com>
2022-04-17 22:08:15 -0300

    Update Sistema.java

--- Sistema.java
+++ Sistema.java
@@ -4,7 +4,9 @@ public class Sistema {
 	static Ventana sistema;
 	
 	public static void main(String [] args) {
+		// Creamos el objeto Facultad que contiene todas las clases (Model y Controller) que nos permiten desarrollar todo el modelo.
 		facultad = new Facultad(); 
-		sistema = new Ventana();
+		// Creamos la ventana que nos permite visualizar los elementos (View)
+		sistema = new Ventana(); 
 	}
 }
